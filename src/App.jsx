0f77f2acca1bcb1322bdb80e7f1596536686e8c4
Ilia Filimonov <9991571155i@gmail.com>
2024-06-11 02:02:52 +0700

    Добавит компоненты Карточка и Степпер с базовой логикой к ним.

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,81 @@
 import "./App.css";
 import { Card } from "./components/ui/Card/Card";
-
-// Данные карточек (продукты)
-const initialProducts = [
-  {
-    id: "1",
-    name: "Armchair",
-    category: "Chair",
-    description:
-      "At IKEA we want to put well-designed, affordable home furnishing within",
-    rating: "4.9",
-    price: "50.2",
-    imgSrc: "../assets/products/product-1.jpg",
-  },
-  {
-    id: "2",
-    name: "Bed",
-    category: "Bed",
-    description:
-      "At IKEA we want to put well-designed, affordable home furnishing within2",
-    rating: "3.0",
-    price: "102.10",
-    imgSrc: "../assets/products/product-2.jpg",
-  },
-  {
-    id: "3",
-    name: "Bench",
-    category: "Bench",
-    description:
-      "At IKEA we want to put well-designed, affordable home furnishing within3",
-    rating: "4.9",
-    price: "15",
-    imgSrc: "../assets/products/product-3.jpg",
-  },
-  {
-    id: "4",
-    name: "Bed 2",
-    category: "Bed 2",
-    description:
-      "At IKEA we want to put well-designed, affordable home furnishing withi4",
-    rating: "3.0",
-    price: "102.10",
-    imgSrc: "../assets/products/product-4.jpg",
-  },
-  {
-    id: "5",
-    name: "Bench2",
-    category: "Bench",
-    description:
-      "At IKEA we want to put well-designed, affordable home furnishing within5",
-    rating: "3.2",
-    price: "12",
-    imgSrc: "../assets/products/product-5.jpg",
-  },
-  {
-    id: "6",
-    name: "Bed2",
-    category: "Bed",
-    description:
-      "At IKEA we want to put well-designed, affordable home furnishing within6",
-    rating: "1.0",
-    price: "105.80",
-    imgSrc: "../assets/products/product-8.jpg",
-  },
-];
+import { initialProducts } from "../data";
+import { useState } from "react";
 
 function App() {
-  const logCardId = (id) => {
-    console.log("Clicked card:", id);
+  // Стейт для продуктов (начальное значение из data.js)
+  const [products, setProducts] = useState(initialProducts);
+
+  // Обработчик клика по карточке (для открытия сайдбара, например)
+  const handleCardBtnClick = (id) => {
+    console.log(`Нажата карточка с ID ${id}`);
+  };
+
+  /**
+   * Обработка изменений в Stepper
+   * @param {string} id - id товара
+   * @param {string} newValue - Новое значение из Stepper
+   */
+  const handleStepperUpdate = (id, newValue) => {
+    const updatedProducts = initialProducts?.map((product) => {
+      if (product?.id === id) {
+        return { ...product, cartQuantity: newValue };
+      }
+
+      return product;
+    });
+
+    // Обновляем стейт
+    setProducts(updatedProducts);
+  };
+
+  /**
+   * Обработка добавления товара в избранное
+   * @param {string} id - id товара
+   */
+  const handleToggleFavorite = (id) => {
+    // Создании поверхностной копии массива
+    const currentProducts = [...products];
+
+    // Находим товара по id
+    const product = currentProducts?.find((product) => product?.id === id);
+
+    if (product) {
+      // Обновляем значение isFavorite у найденного товара
+      product.isFavorite = !product?.isFavorite;
+
+      // Обновляем стейт
+      setProducts(currentProducts);
+
+      // Получаем список избранных товаров
+      const favoriteProducts = currentProducts
+        ?.filter((product) => product?.isFavorite)
+        ?.map((product) => product?.id);
+
+      // Записываем избранные товары в localStorage
+      localStorage.setItem("favorite", JSON.stringify(favoriteProducts));
+    }
+
+    console.log("сохраненки", products);
   };
 
   return (
     <section className="products">
-      <div className="container">
+      <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-between">
-          {!!initialProducts &&
-            initialProducts.map((product) => (
+          {!!products &&
+            products.map((product) => (
               <Card
-                onClick={() => logCardId(product?.id)}
                 key={product?.id}
                 details={product}
+                onBtnClick={handleCardBtnClick}
+                onStepperUpdate={handleStepperUpdate}
+                onToggleFavorite={handleToggleFavorite}
               />
             ))}
         </div>
+        <div></div>
       </div>
     </section>
   );
